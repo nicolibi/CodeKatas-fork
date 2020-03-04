cfa5381660566aea4d0881a3977bde9ec5b82ae8
Nicole Bieger <nicoleregister@email.de>
2020-03-04 23:21:18 +0100

    Leap Year abgeschlossen bis auf Fehler bei Jahr 0

--- src/LeapYear/LeapYear.cs
+++ src/LeapYear/LeapYear.cs
@@ -4,8 +4,9 @@
     {
         public bool IsLeapYear(int i)
         {
+
             // ergänzt am 1.3.20
-            if (i % 4 == 0)
+            if (i >= 8 && i % 4 == 0)
             {
                 if (i % 100 == 0)
                 {
@@ -17,6 +18,35 @@
                 else
                     return true;
             }
+
+            else if (i >= -45 && i <= -9)
+            {
+                if (i % 3 == 0)
+                    return true;
+                else
+                    return false;
+
+            }
+            //keine Schaltjahre zwischen 10 BC und 8 AD
+            else if (i >= -10 && i < 8)
+            {
+                //Sonderregelung für Jahr Null einfügen
+                if (i == 0)
+                {
+                    throw new System.ArgumentException("Year 0 didn't exist");
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            else if (i < -46)
+            {
+                return false;
+            }
+
             else
             {
                 return false;
